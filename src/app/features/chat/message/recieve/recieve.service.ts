9eac93308f165e8aab29a9c7700555aafd23bab2
fuihfuefuiewn <danielbonachelamartinez@gmail.com>
2025-09-19 20:02:19 +0200

    Recieve re-hecho
    Recieve= message history
    Lo que pone en rojo es porque puse los nombres mal y usé componentes en vez de service...
    Mañana más y mejor

---
+++ src/app/features/chat/message/recieve/recieve.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { SERVER_ROUTE } from "../../../../../environment/environment.secret";
+//En el anterior esto no estaba aquí, pero para proceder a la operación rescate es necesario.
+//faltaba por limpiar campos, ya he quitado a la fuerza los que sobraban, ya descubriré porque fallaba
+export interface MessageFormat  {
+  id?: string;
+  sender_id?: string;
+  channel_id?: string;
+  info?: Record<string, string>; 
+  created_at?: string;
+}
+
+@Injectable({providedIn: 'root' })
+    
+export class MessageHistoryService {
+  constructor(private http: HttpClient) {}
+
+getMessages(channelId: string) {
+    const from=0;
+    const to=50;
+    return this.http.get<MessageFormat>(
+        `${SERVER_ROUTE}/api/message/${channelId}?from=${from}&to=${to}`,
+        {
+            headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
+        }
+  );
+}
+}
