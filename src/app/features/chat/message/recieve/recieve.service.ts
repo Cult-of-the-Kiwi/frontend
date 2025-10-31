422c27ee12b80099a11d5a1812b8d5aa33349760
fuihfuefuiewn <danielbonachelamartinez@gmail.com>
2025-10-31 12:42:07 +0100

    Recieve glow up
    Was almost done, for some reason things like id where optional (for sure because it is easier to make it ccompile)
    And there was a problem with routes

--- src/app/features/chat/message/recieve/recieve.service.ts
+++ src/app/features/chat/message/recieve/recieve.service.ts
@@ -4,12 +4,11 @@ import { SERVER_ROUTE } from "../../../../../environment/environment.secret";
 //En el anterior esto no estaba aquí, pero para proceder a la operación rescate es necesario.
 //faltaba por limpiar campos, ya he quitado a la fuerza los que sobraban, ya descubriré porque fallaba
 export interface MessageFormat  {
-  id?: string;
-  sender_id?: string;
-  channel_id?: string;
-  info?: Record<string, string>; 
-  created_at?: string;
-  type?:string;
+  id: string;
+  sender_id: string;
+  channel_id:string;
+  message: string ;
+  created_at:string;
 }
 
 @Injectable({providedIn: 'root' })
@@ -21,7 +20,7 @@ getMessages(channelId: string) {
     const from=0;
     const to=50;
     return this.http.get<MessageFormat[]>(//solo podía devolver un mensaje...jeje (hace falta lista)
-        `${SERVER_ROUTE}/api/message/${channelId}?from=${from}&to=${to}`,
+        `${SERVER_ROUTE}api/message/${channelId}?from=${from}&to=${to}`,
         {
             headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
         }
